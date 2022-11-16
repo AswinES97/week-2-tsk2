884ef0031cf809047a0aaaf20fc14eeb196528f1
aswines <aswinedassery@gmail.com>
2022-11-16 12:25:19 +0530

    validation updated

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,63 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+// validation
+let nameError = document.getElementById("name-error")
+let emailError = document.getElementById("email-error")
+let msgError = document.getElementById("msg-error")
+let submitError = document.getElementById("submit-error")
+
+
+function validateName() {
+    let name = document.getElementById("name").value
+    if (name.length == 0) {
+        nameError.innerHTML = "name field cannot be empty"
+        return false
+    }
+    if (!name.match(/(^[A-Za-z]{3,16})([ ]{0,1})([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})/)) {
+        nameError.innerHTML = "enter full name"
+        return false
+    }
+    nameError.innerHTML = "valid"
+    return true
+}
+
+function validateEmail() {
+    let email = document.getElementById("email").value
+    if (email.length == 0) {
+        emailError.innerHTML = "email field cannot be empty"
+        return false
+    }
+    if (!email.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
+        emailError.innerHTML = "enter correct mail"
+        return false
+    }
+    emailError.innerHTML = "valid"
+    return true
+}
+
+function validateText() {
+    let text = document.getElementById("text").value
+    if (text.length == 0) {
+        msgError.innerHTML = "text field cannot be empty"
+        return false
+    }
+    if (text.length<30) {
+        msgError.innerHTML = "must be 30 characters"
+        return false
+    }
+    msgError.innerHTML = "valid"
+    return true
+}
+
+function submitForm(){
+    if(!validateName()||!validateEmail()||!validateText()){
+        submitError.style.display = "block"
+        submitError.innerHTML = "Please fill all the fields "
+        setTimeout(function (){
+            submitError.style.display = "none"
+        },2000)
+        return false
+    }
+}
